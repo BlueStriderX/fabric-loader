9937c9f9241385d35f57dde5e0e153a0c5446e61
NZSmartie <nzsmartie@gmail.com>
2019-03-23 04:12:20 +1300

    Attempt at geting a test mixins mod working

--- src/test/java/net/fabricmc/test/mixin/MixinGuiMain.java
+++ src/test/java/net/fabricmc/test/mixin/MixinGuiMain.java
@@ -16,19 +16,32 @@
 
 package net.fabricmc.test.mixin;
 
-import net.minecraft.client.gui.MainMenuScreen;
-import net.minecraft.client.gui.Screen;
+import org.schema.common.GraphicsInterface;
+import org.schema.game.client.view.font.FontNode;
+import org.schema.game.client.view.gui.GUIAnchor;
+import org.schema.game.client.view.mainmenu.MainMenuGUI;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = MainMenuScreen.class, remap = false)
-public abstract class MixinGuiMain extends Screen {
+@Mixin(value = MainMenuGUI.class, remap = false)
+public abstract class MixinGuiMain extends GUIAnchor {
+	public MixinGuiMain(GraphicsInterface a) {
+		super(a);
+	}
 
-	@Inject(method = "draw(IIF)V", at = @At("RETURN"))
-	public void draw(int mouseX, int mouseY, float delta, CallbackInfo info) {
-		this.fontRenderer.draw("Fabric Test Mod", 2, this.height - 30, -1);
+	public MixinGuiMain(GraphicsInterface a, float width, float height) {
+		super(a, width, height);
+	}
+
+	@Inject(method = "draw()V", at = @At("RETURN"))
+	public void draw(CallbackInfo info) {
+		FontNode text = new FontNode(10, 10, getState());
+		text.setString("Fabric Test Mod");
+		text.getPos().x = -1.0f;
+        text.getPos().y = getHeight() - 30.0f;
+		// this.a.attach(text);
 	}
 
 }
